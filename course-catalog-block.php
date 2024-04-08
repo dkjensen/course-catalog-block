b7743374ac64473c99f3cfe9fae3e7a9efd927d2
David Jensen <david@dkjensen.com>
2024-04-08 17:00:54 -0400

    feat: declare compatibility with wc hpos

--- course-catalog-block.php
+++ course-catalog-block.php
@@ -188,3 +188,12 @@ function create_block_get_product_course_hours( int $product_id ) {
 
     return $hours;
 }
+
+/**
+ * Declare compatibility with WooCommerce Custom Order Tables
+ */
+add_action( 'before_woocommerce_init', function() {
+	if ( class_exists( \Automattic\WooCommerce\Utilities\FeaturesUtil::class ) ) {
+		\Automattic\WooCommerce\Utilities\FeaturesUtil::declare_compatibility( 'custom_order_tables', __FILE__, true );
+	}
+} );
